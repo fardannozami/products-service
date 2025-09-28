28f92b5868908dc90181d2d434d8b3b4882e2e84
nozami <fardannozami@gmail.com>
2025-09-28 17:00:09 +0700

    adding redis event listener

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,22 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  const tcpMicroservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
       host: '127.0.0.1',
       port: 4001,
     }
   });
-  await app.listen();
-  console.log('products-service is listening on port 4001');
+  const redisMicroservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: '127.0.0.1',
+      port: 6379,
+    }
+  });
+
+  await Promise.all([tcpMicroservice.listen(), redisMicroservice.listen()]);
+  console.log('products-service is listening on port 4001 and listen to redis event');
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,16 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 
 @Controller('products')
 export class ProductsController {
     @MessagePattern('get_product')
     getProduct(id: number) {
         return { id, name: 'Product 1', price: 1000 };
+    }
+
+    @EventPattern('order.created')
+    async updateStock(order: { id: number; productId: number; }) {
+        console.log( 'Checking stock for product', order.productId);
+        console.log({message: 'Stock updated'});
     }
 }
